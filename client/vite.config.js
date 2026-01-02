438f63bce16dcab76842ddfafd6b8a9a8083f30f
Abdullah Faruk G <afgonullu@gmail.com>
2026-01-02 17:53:13 +0100

    xyz

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: [""],
     proxy: {
       // Forward same-origin GraphQL requests to the backend during `vite dev`.
       '/graphql': {
